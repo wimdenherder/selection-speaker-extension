94b7c043cbc74bd0a87c7d6c91ef3f619ac7b8a8
wimdenherder <wimdenherder@gmail.com>
2023-02-04 06:38:20 +0100

    Fix const lang

--- src/execute.js
+++ src/execute.js
@@ -9,7 +9,7 @@ async function main() {
   let text = getSelectionText() || getContentOfArticle();
 
   const detectedLanguage = await detectLanguage(text);
-  const lang = window.prompt("Enter language code (e.g. nl = netherlands, en = english). It will be translated automatically. ", detectedLanguage);
+  let lang = window.prompt("Enter language code (e.g. nl = netherlands, en = english). It will be translated automatically. ", detectedLanguage);
   if(!lang) return console.log('No language code entered.');
   
   if(lang[lang.length-1] === "!") { // with ! at the end, it will be translated with the learning algorithm!
